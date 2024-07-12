779086aa42f03b417a78490967c4b483f3f2ed07
mutinduhh <jonathan.mutinda@student.moringaschool.com>
2024-07-12 09:38:15 +0300

    updates

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards(names,gift) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+  }
+  function countDown(number) {
+          for (let e = number; e >= 0; e--) {
+            console.log(e);
+          }
+        }
+  
